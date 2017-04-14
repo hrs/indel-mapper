34afc2f08d13584b54e45921001f2ac45aff92ba
Jenny <jenny@caretdashcaret.com>
2017-04-13 21:42:51 -0700

    Limit the size of upload to the server.

--- app.py
+++ app.py
@@ -2,21 +2,23 @@ from flask import Flask, render_template, request, flash
 from indel_mapper_lib.sam_parser import SamParser
 from indel_mapper_lib.reference_parser import ReferenceParser
 from indel_mapper_lib.presenter import Presenter
-import os
 from io import StringIO
 import csv
+import os
 import pysam
 
 # Flask
 
 app = Flask(__name__)
 app.secret_key = os.environ["SECRET_FLASK_KEY"]
+app.config['MAX_CONTENT_LENGTH'] = 20 * 1024 * 1024
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         reference_file = request.files.get('reference')
         alignment_file = request.files.get('alignment')
+
         if not (alignment_file and reference_file):
             flash("You must submit an alignment SAM file and a reference CSV file.")
         elif not _is_extension(reference_file, ".csv"):
@@ -31,6 +33,11 @@ def index():
                 flash("Error processing.")
                 return render_template("index.html", results=[])
     return render_template("index.html", results=[])
+
+@app.errorhandler(413)
+def request_entity_too_large(error):
+    flash("Uploaded file is too large.")
+    return render_template("index.html", results=[]), 413
 
 # This is a first pass naive file detection.
 def _is_extension(filestorage, extension):

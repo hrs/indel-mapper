84cfb57c662a54beee12eeb846f4ce5a613cfdd2
Jenny <jenny@caretdashcaret.com>
2017-04-22 20:21:07 -0700

    Move JS into head.

--- static/script.js
+++ static/script.js
@@ -1,44 +1,46 @@
+MAX_ALIGNMENT_FILE_SIZE = 16 * 1024 * 1024; // 16MB
+MAX_REFERENCE_FILE_SIZE = 3 * 1024 * 1024; // 3MB
+
 function endsWith(str, suffix) {
     return str.indexOf(suffix, str.length - suffix.length) !== -1;
 }
 
 function showMessage(message, cssClass) {
-	clearMessages(cssClass);
-	$(".flashes").append("<ul class='" + cssClass + "'><li>" + message + "</li></ul>");
+    clearMessages(cssClass);
+    $(".flashes").append("<ul class='" + cssClass + "'><li>" + message + "</li></ul>");
 }
 
 function clearMessages(cssClass) {
-	$("." + cssClass).remove();
+    $("." + cssClass).remove();
 }
 
 function checkUploadFiles(target, description, fileExtension, maxSize, cssClass) {
-	clearMessages("server-error")
-	if (target[0].files[0].size > maxSize) {
-		showMessage('The ' + description + ' file is too large.', cssClass);
-	} else if (!endsWith(target.val().toLowerCase(), fileExtension)) {
-		console.log(target.val().toLowerCase());
-		showMessage('The ' + description + ' file must be a ' + fileExtension + ' file.', cssClass);
-	} else {
-		clearMessages(cssClass);
-	}
+    clearMessages("server-error");
+    if (target[0].files[0].size > maxSize) {
+        showMessage('The ' + description + ' file is too large.', cssClass);
+    } else if (!endsWith(target.val().toLowerCase(), fileExtension)) {
+        showMessage('The ' + description + ' file must be a ' + fileExtension + ' file.', cssClass);
+    } else {
+        clearMessages(cssClass);
+    }
 }
-
-$('[name=alignment]').bind('change', function() {
-	target = $('[name=alignment]');
-	maxSize = 16 * 1024 * 1024; // 16MB
-	checkUploadFiles(target, 'alignment', '.sam', maxSize, 'alignment-file-error')
-});
-
-$('[name=reference]').bind('change', function() {
-	target = $('[name=reference]');
-	maxSize = 3 * 1024 * 1024; // 3MB
-	checkUploadFiles(target, 'reference', '.csv', maxSize, 'reference-file-error')
-});
 
 $(function() {
     $(".accordion").accordion({
         animate: false,
         collapsible: true,
         heightStyle: "content",
+    });
+});
+
+$(document).ready(function() {
+    $('[name=alignment]').bind('change', function() {
+        target = $('[name=alignment]');
+        checkUploadFiles(target, 'alignment', '.sam', MAX_ALIGNMENT_FILE_SIZE, 'alignment-file-error');
+    });
+
+    $('[name=reference]').bind('change', function() {
+        target = $('[name=reference]');
+        checkUploadFiles(target, 'reference', '.csv', MAX_REFERENCE_FILE_SIZE, 'reference-file-error');
     });
 });

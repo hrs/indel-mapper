a99619ac98c7eab988324f75c4247b7a7907bba3
Harry Schwartz <hello@harryrschwartz.com>
2017-04-23 21:22:59 -0700

    Fix bugs

--- indel_mapper_lib/tabular_reference_exporter.py
+++ indel_mapper_lib/tabular_reference_exporter.py
@@ -1,21 +1,22 @@
 class TabularReferenceExporter(object):
     HEADER = ["Name", "Sequence", "N20", "PAM", "Total Reads", "Cutsite",
               "Count", "Reference", "Read"]
+
     def __init__(self, reference_presenters):
         self.reference_presenters = reference_presenters
 
     def export(self):
-        rows = [HEADER]
+        rows = [self.HEADER]
         for ref in self.reference_presenters:
-            rows.extend(_rows_for(ref))
+            rows.extend(self._rows_for(ref))
         return rows
 
     def _rows_for(self, ref):
         rows = []
-        prefix_cells = reference_presenter.csv_row_prefix_cells()
+        prefix_cells = ref.csv_row_prefix_cells()
 
-        if reference_presenter.has_mutation_clusters():
-            for cluster in reference_presenter.mutation_clusters:
+        if ref.has_mutation_clusters():
+            for cluster in ref.mutation_clusters:
                 cluster_info = [cluster.cutsite_region, cluster.count()]
                 for sequence in cluster.representations:
                     rows.append(prefix_cells + cluster_info +

f412b2f3d942c99fc70a9a21828213b28e189458
Harry Schwartz <hello@harryrschwartz.com>
2017-05-25 20:30:15 -0700

    Extract a general-purpose AwsUpload class
    CsvUpload is nice, but it automatically generates an obscure destination
    filename for us (which is a `.csv`), and we don't always want that.
    
    This commit extracts a more general `AwsUpload` class and reimplements
    `CsvUpload` in terms of it.

--- indel_mapper_lib/csv_upload.py
+++ indel_mapper_lib/csv_upload.py
@@ -1,43 +1,12 @@
-import boto3
 import datetime
-import os
+from indel_mapper_lib.aws_upload import AwsUpload
 
 
 class CsvUpload(object):
-    BUCKET_NAME = "indel-mapper"
-
     def __init__(self, s3_access_key, s3_secret_key, file):
-        self.s3_access_key = s3_access_key
-        self.s3_secret_key = s3_secret_key
-        self._try_uploading(file)
-
-    def _try_uploading(self, file):
-        filename = self._csv_filename()
-        try:
-            self._upload(file, filename)
-            self._make_public(filename)
-
-            self.succeeded = True
-            self.url = self._url_for(filename)
-        except Exception as e:
-            print(e)
-            self.succeeded = False
-            self.url = None
-
-    def _s3(self):
-        return boto3.resource("s3",
-                              aws_access_key_id=self.s3_access_key,
-                              aws_secret_access_key=self.s3_secret_key)
-
-    def _upload(self, file, filename):
-        self._s3().Bucket(self.BUCKET_NAME).upload_fileobj(file, filename)
-
-    def _make_public(self, filename):
-        object_acl = self._s3().ObjectAcl(self.BUCKET_NAME, filename)
-        object_acl.put(ACL="public-read")
-
-    def _url_for(self, filename):
-        return "https://{}.s3.amazonaws.com/{}".format(self.BUCKET_NAME, filename)
+        upload = AwsUpload(s3_access_key, s3_secret_key, file, self._csv_filename())
+        self.succeeded = upload.succeeded
+        self.url = upload.url
 
     def _csv_filename(self):
         now = datetime.datetime.now()

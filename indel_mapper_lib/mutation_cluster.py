3e927efeddbe3436c87009848f7c6af4d3b0aae4
Jenny <jenny@caretdashcaret.com>
2017-05-27 09:19:33 -0700

    Remove from_dict initializer since we have tmp json.

--- indel_mapper_lib/mutation_cluster.py
+++ indel_mapper_lib/mutation_cluster.py
@@ -2,22 +2,16 @@ from .sequence_diff import SequenceDiff
 from .alignment import Alignment
 
 class MutationCluster(object):
-    def __init__(self, marked_alignment=None, marked_cas9_region=None, from_dict=None):
-        print(from_dict)
-        if from_dict is not None:
-            self._init_from_dict(from_dict)
-        else:
-            self.cas9_region = marked_cas9_region
-            self.alignments = [marked_alignment]
-            self.description = SequenceDiff(marked_cas9_region.reference, marked_cas9_region.read).description()
-            self._count = len(self.alignments)
+    def __init__(self, marked_alignment, marked_cas9_region):
+        self.cas9_region = marked_cas9_region
+        self.alignments = [marked_alignment]
+        self.description = SequenceDiff(marked_cas9_region.reference, marked_cas9_region.read).description()
 
     def add_read(self, representation):
         self.alignments.append(representation)
-        self._count = len(self.alignments)
 
     def count(self):
-        return self._count
+        return len(self.alignments)
 
     def csv_row(self):
         return [self.cas9_region.reference, self.cas9_region.read, self.description, self.count()]
@@ -47,9 +41,3 @@ class MutationCluster(object):
             "description": self.description,
             "alignments": [alignment.to_dict() for alignment in self.alignments]
         }
-
-    def _init_from_dict(self, mutation_cluster_dict):
-        self._count = mutation_cluster_dict["count"]
-        self.description = mutation_cluster_dict["description"]
-        self.cas9_region = Alignment(from_dict=mutation_cluster_dict["cas9_region"])
-        self.alignments = [Alignment(from_dict=alignment_dict) for alignment_dict in mutation_cluster_dict["alignments"]]
--- indel_mapper_lib/reference_presenter.py
+++ indel_mapper_lib/reference_presenter.py
@@ -1,16 +1,13 @@
 from .mutation_cluster import MutationCluster
 
 class ReferencePresenter(object):
-    def __init__(self, reference=None, mutation_clusters=None, from_dict=None):
-        if from_dict is not None:
-            self._init_from_dict(from_dict)
-        else:
-            self.mutation_clusters = mutation_clusters
-            self._name = reference.name
-            self._sequence = reference.sequence
-            self._n20 = reference.n20
-            self._pam = reference.pam
-            self._total_reads = len(reference.reads)
+    def __init__(self, reference, mutation_clusters):
+        self.mutation_clusters = mutation_clusters
+        self._name = reference.name
+        self._sequence = reference.sequence
+        self._n20 = reference.n20
+        self._pam = reference.pam
+        self._total_reads = len(reference.reads)
 
     def name(self):
         return self._name
@@ -43,11 +40,3 @@ class ReferencePresenter(object):
             "total_reads": self.total_reads(),
             "mutation_clusters": [cluster.to_dict() for cluster in self.mutation_clusters]
         }
-
-    def _init_from_dict(self, reference_presenter_dict):
-        self._name = reference_presenter_dict["name"]
-        self._sequence = reference_presenter_dict["sequence"]
-        self._pam = reference_presenter_dict["pam"]
-        self._n20 = reference_presenter_dict["n20"]
-        self._total_reads = reference_presenter_dict["total_reads"]
-        self.mutation_clusters = [MutationCluster(from_dict=cluster_dict) for cluster_dict in reference_presenter_dict["mutation_clusters"]]
